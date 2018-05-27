1c1b324ad83a81d48c2ddd60074c2668d80f4f89
sinnedsynd <39647623+sinnedsynd@users.noreply.github.com>
2018-05-27 17:01:55 +0800

    delete

--- AChecker.h
+++
@@ -1,9 +0,0 @@
-#include "LetterChecker.h"
-
-class AChecker : public LetterChecker
-{
-	//override the function "checkMyLetter"	
-	//create the function declaration here
-public:
-	virtual char checkMyLetter (char inputChar);
-};
