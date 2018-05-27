8973c372d22970556484fab616b49bfd8a81ed4f
sinnedsynd <39647623+sinnedsynd@users.noreply.github.com>
2018-05-27 16:59:48 +0800

    commit aChecker
    achecker

---
+++ AChecker.h
@@ -0,0 +1,9 @@
+#include "LetterChecker.h"
+
+class AChecker : public LetterChecker
+{
+	//override the function "checkMyLetter"	
+	//create the function declaration here
+public:
+	virtual char checkMyLetter (char inputChar);
+};
